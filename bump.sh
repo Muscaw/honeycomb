6dfe574c74d7320754e8a2a6fd17718162895216
Kevin Grandjean <moi@kevingrandjean.ch>
2025-12-07 18:25:16 +0700

    Add bump.sh to manage versions

---
+++ bump.sh
@@ -0,0 +1,23 @@
+version="$1"
+
+if ! [[ "$version" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+  echo "Version does not match x.y.z" >&2
+  exit 1
+fi
+
+# Verify we are currently at the top of the main branch
+
+git fetch origin main
+if [[ $(git rev-list --count HEAD..@{u}) != 0 ]]; then
+  echo "Current git HEAD is not up-to-date with upstream" >&2
+  exit 1
+fi
+
+uv version $version
+
+git add pyproject.toml uv.lock
+
+git commit -m "Bump project version"
+git tag $version
+
+echo "Ready to push tag"
